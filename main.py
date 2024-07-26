79880273fba239a77d3807c0e77a286f7bbceefe
6elosnezhka <nikroll6@gmail.com>
2024-07-26 16:45:02 +0800

    ifelse выполнил

--- main.py
+++ main.py
@@ -166,3 +166,52 @@
 #     for i in range(len(string)):
 #         print(i)
 # count_AB("ABABAB")
+
+
+# 1ифелсе
+
+# a = int(input("Введите число: "))
+# if a % 2==0:
+#     print("четное")
+# else:
+#     print("нечетное")
+
+# 2
+# a = int(input("Введите число: "))
+# if a < 0:
+#     print("число меньше нуля")
+# elif a > 0:
+#     print("число больше нуля")
+# else:
+#     print("число равно нулю")
+
+# 3
+# score = int(input("Введите число: "))
+# if 90 <= score <= 100:
+#     print("A")
+# if 80 <= score < 90:
+#     print("B")
+# if 70 <= score < 80:
+#     print("C")
+# if 60 <= score < 70:
+#     print("D")
+# if 0 <= score < 60:
+#     print("F")
+
+# 4
+# year = int(input("Введите год: "))
+# if year % 4 == 0:
+#     print("Этот год високосный.")
+# else:
+#     print("Этот год не является високосным.")
+
+# 5
+# frst_num = float(input("Введите первое число: "))
+# sec_num = float(input("Введите второе число: "))
+# if frst_num > sec_num:
+#     print("Первое число больше")
+# elif frst_num < sec_num:
+#     print("Второе число больше")
+# else:
+#     print("Числа равны")
+
